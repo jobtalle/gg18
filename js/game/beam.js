813ce284f8e0219a9a2e488a92b4901dfbe089f3
= <jobtalle@hotmail.com>
2018-01-27 00:25:08 +0100

    Added missing files

---
+++ js/game/beam.js
@@ -0,0 +1,14 @@
+function Beam(angle, color) {
+    this.angle = angle;
+    this.color = color;
+}
+
+Beam.prototype = {
+    update(timeStep) {
+        
+    },
+    
+    render(context) {
+        
+    }
+}
