efa1d8c1b616451105a58a4df2909717a13e4fe4
Jurjen Biewenga <j.f.biewenga@gmail.com>
2018-01-27 00:22:25 +0100

    Added fancy planet graphics

--- js/game/planet.js
+++ js/game/planet.js
@@ -5,15 +5,16 @@ function Planet() {
 }
 
 Planet.prototype = {
-    COLOR_PLANET: "#399e11",
-    COLOR_PLANET_HIGHLIGHT: "#55FF11",
+    COLOR_PLANET: "#c0d0b0",
+    COLOR_PLANET_HIGHLIGHT: "#dae4d0",
+    COLOR_PLANET_LINES: "#afc0b5",
     RADIUS: 100,
     RADIUS_ORBIT: 150,
     RADIUS_INCOMING: 400,
     ROTATION_SPEED: 0.1,
     SCENERY_AMOUNT: 40,
     ATMOSPHERE_SIZE_MODIFIER: 1.2,
-    ATMOSPHERE_COLOR: "#16d6e0",
+    ATMOSPHERE_COLOR: "rgba(89,133,221,0.66)",
     ATMOSPHERE_START_ANGLE: 2.35619449019,
     ATMOSPHERE_END_ANGLE: -0.78539816339,
     
@@ -33,6 +34,7 @@ Planet.prototype = {
         context.restore();
 
         this.renderHighlight(context);
+        this.renderLines(context);
         
         context.save();
         context.rotate(this.angle);        
@@ -65,9 +67,9 @@ Planet.prototype = {
     {
         context.save();
 
-        grd=context.createRadialGradient(0,0,this.RADIUS - 10,0,0,130);
-        grd.addColorStop(0, this.ATMOSPHERE_COLOR);
-        grd.addColorStop(1,"#00000000");
+        grd=context.createRadialGradient(0,0,this.RADIUS,0,0,130);
+        grd.addColorStop(0,"#00000000");
+        grd.addColorStop(1, this.ATMOSPHERE_COLOR);
 
         context.beginPath();
         context.fillStyle = grd;
@@ -80,19 +82,56 @@ Planet.prototype = {
     renderHighlight(context){
         context.save();
 
-        var grd=context.createRadialGradient(-30,-50,100,-30,-50,300);
+        var grd=context.createRadialGradient(-30,-50,100,-30,-50,120);
         grd.addColorStop(0, this.COLOR_PLANET_HIGHLIGHT);
-        grd.addColorStop(1,"#00000000");
+        grd.addColorStop(1,"rgba(0,0,0,0)");
 
         context.beginPath();
-        context.fillStyle ="#00000000";
+        context.fillStyle ="rgba(0,0,0,0)";
         context.arc(0,0, this.RADIUS,  0, Math.PI * 2);
         context.fill();
         context.clip();
 
         context.beginPath();
-        context.fillStyle = grd;
+        context.fillStyle = this.COLOR_PLANET_HIGHLIGHT;
+        context.arc(-30,-50, this.RADIUS * 1.2,  0, Math.PI * 2);
+        context.fill();
+
+        context.restore();
+    },
+
+    renderLines(context)
+    {
+        context.save();
+
+        context.beginPath();
+        context.fillStyle ="rgba(0,0,0,0)";
         context.arc(0,0, this.RADIUS,  0, Math.PI * 2);
+        context.fill();
+        context.clip();
+
+        context.beginPath();
+        context.fillStyle = this.COLOR_PLANET_LINES;
+
+        context.rect(20,40,200,2);
+        context.rect(40,34,200,2);
+
+        context.rect(80,-20,200,2);
+        context.rect(60,-14,30,2);
+        
+        context.rect(30,-40,200,2);
+        context.rect(10,-46,30,2);
+
+        context.rect(-100,46,30,2);
+
+        context.rect(-100,-30,60,2);
+
+        context.fill();
+
+        context.clip();
+        context.beginPath();
+        context.fillStyle = this.COLOR_PLANET;
+        context.arc(-30,-50,this.RADIUS * 1.2, 0, Math.PI * 2);
         context.fill();
 
         context.restore();
