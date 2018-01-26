e721400aedeb02959ba841e3c5e4e8a4b6940d3e
= <jobtalle@hotmail.com>
2018-01-26 21:32:50 +0100

    Created fancy ufo movement

--- js/game/ufo.js
+++ js/game/ufo.js
@@ -1,7 +1,9 @@
 function Ufo() {
-    this.angle = Math.random() * Math.PI * 2;
-    this.orbits = 2;
-    this.speed = this.getRadialSpeed(200);
+    this.initialAngle = Math.random() * Math.PI * 2;
+    this.angle = 0;
+    this.orbit = 0;
+    this.orbits = 1;
+    this.speed = this.getRadialSpeed(400);
 }
 
 Ufo.prototype = {
@@ -10,17 +12,23 @@ Ufo.prototype = {
     
     update(timeStep) {
         const radius = this.getRadius();
+        const angle = this.angle + this.initialAngle;
+        
+        if(this.angle > Math.PI * 2) {
+            this.angle -= Math.PI * 2;
+            ++this.orbit;
+        }
         
         this.angle += this.speed * timeStep;
         this.position = new Vector(
-            Math.cos(this.angle) * radius,
-            Math.sin(this.angle) * radius);
+            Math.cos(angle) * radius,
+            Math.sin(angle) * radius);
     },
     
     render(context) {
         context.save();
         context.translate(this.position.x, this.position.y);
-        context.rotate(this.angle + Math.PI * 0.5);
+        context.rotate(this.initialAngle + this.angle + Math.PI * 0.5);
         
         context.strokeStyle = this.COLOR;
         context.beginPath();
@@ -37,6 +45,12 @@ Ufo.prototype = {
     },
     
     getRadius() {
+        if(this.orbit == 0 && this.angle < this.INCOMING_RADIANS) {
+            return Planet.prototype.RADIUS_ORBIT +
+                (Planet.prototype.RADIUS_INCOMING - Planet.prototype.RADIUS_ORBIT) *
+                (0.5 + Math.cos((this.angle / this.INCOMING_RADIANS) * Math.PI) * 0.5);
+        }
+        
         return Planet.prototype.RADIUS_ORBIT;
     },
     
