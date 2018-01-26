ba98a513421bfd6a55f9a510cb2a5ec623944a4e
Jurjen Biewenga <j.f.biewenga@gmail.com>
2018-01-27 00:22:12 +0100

    Made space invisible

--- js/game/space.js
+++ js/game/space.js
@@ -6,8 +6,7 @@ Space.prototype = {
     COLOR_SPACE: "#001144",
     
     render(context) {
-        context.fillStyle = this.COLOR_SPACE;        
-        context.fillRect(0, 0, context.canvas.clientWidth, context.canvas.clientHeight);
+        context.clearRect(0,0, context.canvas.clientWidth, context.canvas.clientHeight);
     },
     
     update(timeStep) {
