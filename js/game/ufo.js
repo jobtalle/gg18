74ead21990d997b8657fec4a963c4767cb8c1e8a
= <jobtalle@hotmail.com>
2018-01-27 16:39:48 +0100

    Added debug text

--- js/game/ufo.js
+++ js/game/ufo.js
@@ -14,8 +14,17 @@ Ufo.prototype = {
     },
     
     render(context) {
+        context.fillStyle = "white";
+        
+        var colorTexts = "";
+        for(var i = 0; i < this.colors.length; ++i)
+            colorTexts += this.colors[i] + " ";
+        
+        context.fillText(colorTexts, this.mover.position.x + 16, this.mover.position.y);
+        
         context.save();
         context.translate(this.mover.position.x, this.mover.position.y);
+        
         context.rotate(this.mover.getAngle() + Math.PI * 0.5);
         
         context.strokeStyle = this.COLOR;
