d53bd90ded921bb77f6ef8e5b756341992c32df7
Bram Dingelstad <bram@oneartplea.se>
2018-01-28 00:31:32 +0100

    Added a restart function to the game

--- js/game/game.js
+++ js/game/game.js
@@ -4,7 +4,7 @@ function Game(renderer, players) {
     this.planet = new Planet(players);
     this.dispatcher = new UfoDispatcher(this.planet.dispatch.bind(this.planet));
     this.dispatcher.start();
-    
+
     this.shakeZoom = 0;
     this.shakeAngle = 0;
     this.shakeAngleSpeed = 0;
@@ -12,51 +12,55 @@ function Game(renderer, players) {
 
 Game.prototype = {
     SCALE: 4,
-    
+
     start() {
         this.lastDate = new Date();
         this.animate();
     },
-    
+
+    restart() {
+        document.location.reload();
+    },
+
     animate() {
         requestAnimationFrame(this.animate.bind(this));
-        
+
         this.update(this.getTimeStep());
         this.render(this.renderer.getContext());
     },
-    
+
     getTimeStep() {
         var date = new Date();
         var timeStep = (date - this.lastDate) / 1000;
-        
+
         if(timeStep < 0)
             timeStep += 1.0;
-        
+
         this.lastDate = date;
-        
+
         return timeStep;
     },
-    
+
     shake() {
         this.shakeZoom += 0.1;
         this.shakeAngleSpeed = -this.shakeAngleSpeed + Math.sign(this.shakeAngleSpeed) * 1;
     },
-    
+
     update(timeStep) {
         this.space.update(timeStep);
         this.dispatcher.update(timeStep);
         this.planet.update(timeStep);
-        
+
         this.shakeAngle += this.shakeAngleSpeed;
         this.shakeAngle *= 1 - 0.1 * timeStep;
         this.shakeZoom *= 1 - 0.1 * timeStep;
     },
-    
+
     renderBackground(context) {
         context.restore();
-        
+
         this.space.render(context);
-        
+
         context.save();
         context.translate(
             context.canvas.width / 2,
@@ -64,22 +68,22 @@ Game.prototype = {
         context.scale(this.SCALE + this.shakeZoom, this.SCALE + this.shakeZoom);
         context.rotate(this.shakeAngle);
     },
-    
+
     translateContext(context, offset) {
         context.save();
-        
+
         context.translate(
             offset.x,
             offset.y
         );
-        
+
         context.restore();
     },
-    
+
     renderGame(context) {
         this.planet.render(context);
     },
-    
+
     render(context) {
         this.renderBackground(context);
         this.renderGame(context);
